1b38cf2f7f99fbaaf0dc97cbbced70b3e9c6f18d
dan1kali <dl74@rice.edu>
2025-01-28 19:31:16 -0600

    updated import serial files to csv

---
+++ import_serial_csv.py
@@ -0,0 +1,24 @@
+import serial
+import csv
+
+ser = serial.Serial('/dev/cu.usbmodem14201', 115200)
+
+# Open a CSV file to log the data
+with open('/Users/macbook/Desktop/Arduino/serial_output.csv', 'w', newline='') as file:
+    csv_writer = csv.writer(file)
+    
+    # Write the header for CSV (optional)
+    csv_writer.writerow(['Time (s)', 'Fluid Force (mN)', 'Weight 1 (g)', 'Weight 2 (g)'])
+    
+    while True:
+        if ser.in_waiting > 0:
+            data = ser.readline().decode('utf-8').strip()
+            
+            # Check if the line contains only numeric data (by checking for commas and valid floats)
+            try:
+                values = [float(val) for val in data.split(',')]
+                
+                csv_writer.writerow(values)
+                print(data)
+            except:
+                continue
--- import_serial_txt.py
+++ import_serial_txt.py
@@ -2,10 +2,10 @@
 
 import serial
 
-ser = serial.Serial('/dev/cu.usbmodem14201', 57600)
+ser = serial.Serial('/dev/cu.usbmodem14201', 115200)
 
 # Open a file to log the data
-with open('serial_output.txt', 'w') as file:
+with open('/Users/macbook/Desktop/Arduino/serial_output.txt', 'w') as file:
     while True:
         if ser.in_waiting > 0:
             data = ser.readline().decode('utf-8').strip()
